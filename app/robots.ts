8fc135bdf72876ece34562dbbf6736819ab8a89f
Abcfallsikring <finn@abcfallsikring.no>
2026-05-21 09:45:32 +0200

    Update robots.txt rules and sitemap URL

--- app/robots.ts
+++ app/robots.ts
@@ -1,15 +1,14 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
+  const baseUrl = 'https://v0-h-ydesikkerhet.vercel.app' // Bytt ut med endelig domene
+
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/api/', '/_next/'],
-      },
-    ],
-    sitemap: 'https://www.abcfallsikring.no/sitemap.xml',
-    host: 'https://www.abcfallsikring.no',
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/admin/', '/api/'], // Sider Googlebot skal holde seg unna
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
   }
 }
