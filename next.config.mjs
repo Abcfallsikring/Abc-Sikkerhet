eb7d45e28592f84e782ecf533144a0e43c541e25
Abcfallsikring <finn@abcfallsikring.no>
2026-07-21 11:14:42 +0200

    Add rewrites for specific HTML pages

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async rewrites() {
+    return [
+      { source: '/arlig-kontroll', destination: '/arlig-kontroll.html' },
+      { source: '/bedriftsintern-kurs', destination: '/bedriftsintern-kurs.html' },
+      { source: '/oslo', destination: '/oslo.html' },
+      { source: '/nord-norge', destination: '/nord-norge.html' },
+    ]
+  },
 }
 
 export default nextConfig
